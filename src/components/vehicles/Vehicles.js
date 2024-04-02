c45e87cb0fa2d83b59f813b2f7a1ab5542c2b936
thealamdev <shahalam141072@gmail.com>
2024-04-02 22:36:35 +0600

    done

--- src/components/vehicles/Vehicles.js
+++ src/components/vehicles/Vehicles.js
@@ -4,17 +4,36 @@ import React, { useEffect, useState } from 'react'
 export default function Vehicles(props) {
 
     const [vehicles, setVehicles] = useState([]);
+    const [count, setCount] = useState(1);
 
     useEffect(() => {
-        axios.get('https://pilotbazar.com/api/clients/vehicles/products?page=2')
-            .then(response => {
-                setVehicles(response.data.payload.data);
-            })
-            .catch(error => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`https://pilotbazar.com/api/clients/vehicles/products?page=${count}`);
+                setVehicles(prevVehicles => [...prevVehicles, ...response.data.payload.data]);
+            } catch (error) {
                 console.log(error);
-            }, [])
+            }
+        };
 
-    })
+        fetchData();
+
+    }, [count]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.scrollHeight) {
+                setCount(prevCount => prevCount + 1);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
 
     const vehicleItem = vehicles.map((item, index) => {
         return (
