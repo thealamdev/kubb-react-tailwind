8372f7038c6a938030cbd98f59971499b1cd3c8e
thealamdev <shahalam141072@gmail.com>
2024-04-03 23:47:32 +0600

    done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <div className='text-center py-10 dark:bg-slate-800 bg-gray-900 text-slate-100 dark:text-slate-100'>
-            @copyright,2024 all right reserved Shah Alam
+            <h1 className='hover:rotate-0'> @copyright,2024 all right reserved Shah Alam</h1>
         </div>
     )
 }
