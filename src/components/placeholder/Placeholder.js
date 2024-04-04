421c8ad30c86454aa40723bee3305a4dac336530
thealamdev <shahalam141072@gmail.com>
2024-04-04 17:13:17 +0600

    done

--- src/components/placeholder/Placeholder.js
+++ src/components/placeholder/Placeholder.js
@@ -7,135 +7,134 @@ export default function Placeholder() {
             <div class="container pt-20 grid grid-cols-3 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 rounded shadow animate-pulse dark:divide-gray-700 dark:border-gray-700">
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
 
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
                 <div className='space-y-1'>
-                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-44 mb-2.5"></div>
+                    <div class="h-36 bg-gray-300 rounded dark:bg-gray-600 w-46 mb-2.5"></div>
                     <div class="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-24 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                     <div class="w-12 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 </div>
-
 
                 <span class="sr-only">Loading...</span>
             </div>
